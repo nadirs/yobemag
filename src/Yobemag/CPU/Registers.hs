7dbc3cee8608ac103f6ad8c615a8bdf9afe0685f
nadirs <nadirsampaoli@gmail.com>
2014-08-03 01:32:26 +0200

    Rewrite Registers in a more flat way

--- src/Yobemag/CPU/Registers.hs
+++ src/Yobemag/CPU/Registers.hs
@@ -1,47 +1,70 @@
-module Yobemag.CPU.Registers where
+module Yobemag.CPU.Registers
+    ( CPUState(..)
+    , Reg8(..)
+    , Reg16(..)
+    , RegsState(RegsState)
+    , getReg8
+    , setReg8
+    , getReg16
+    , setReg16
+    ) where
 
-import Data.List (intercalate)
 import Yobemag.Binary
 
-{--
- -- Flags:
- -- z  → zero
- -- c  → carry
- -- h → half-carry
- -- n → negative
- --}
-type Zero = Bool
-type Carry = Bool
-type HalfCarry = Bool
-type Neg = Bool
-newtype Flags = Flags
-              { getFlags :: (Zero, Carry, HalfCarry, Neg)
-              } deriving (Eq)
+data Reg8 = A | F | B | C | D | E | H | L deriving Show
+data Reg16 = AF | BC | DE | HL deriving Show
 
-instance Show Flags where
-    show (Flags (z, c, h, n)) = intercalate " | " . map (uncurry showf) $ [(z, "Z"), (c, "C"), (h, "H"), (n, "N")]
-      where
-        showf True s = s
-        showf False s = 'N' : s
+data CPUState = CPUState
+    { regsState :: RegsState
+    } deriving Show
 
-data Registers = Registers
-               { a :: Byte
-               , f :: Flags
-               , bc :: Word
-               , de :: Word
-               , hl :: Word
-               , sp :: Word
-               } deriving (Show, Eq)
+data RegsState = RegsState
+    { regA :: Byte
+    , regF :: Byte
+    , regB :: Byte
+    , regC :: Byte
+    , regD :: Byte
+    , regE :: Byte
+    , regH :: Byte
+    , regL :: Byte
+    } deriving Show
 
-emptyRegisters :: Registers
-emptyRegisters = Registers
-              { a = 0
-              , f = emptyFlags
-              , bc = 0
-              , de = 0
-              , hl = 0
-              , sp = 0
-              }
+getReg8 :: Reg8 -> RegsState -> Byte
+getReg8 r = case r of
+    A -> regA
+    F -> regF
+    B -> regB
+    C -> regC
+    D -> regD
+    E -> regE
+    H -> regH
+    L -> regL
 
-emptyFlags :: Flags
-emptyFlags = Flags (False, False, False, False)
+setReg8 :: Reg8 -> Byte -> RegsState -> RegsState
+setReg8 r n s = case r of
+    A -> s { regA = n }
+    F -> s { regF = n }
+    B -> s { regB = n }
+    C -> s { regC = n }
+    D -> s { regD = n }
+    E -> s { regE = n }
+    H -> s { regH = n }
+    L -> s { regL = n }
+
+splitRegs :: (Reg8 -> Reg8 -> a) -> Reg16 -> a
+splitRegs f r = case r of
+    AF -> f A F
+    BC -> f B C
+    DE -> f D E
+    HL -> f H L
+
+getReg16 :: Reg16 -> RegsState -> Word
+getReg16 r s = splitRegs pair r where
+    pair x y = toWord (getReg y) (getReg x)
+    getReg x = getReg8 x s
+
+setReg16 :: Reg16 -> Word -> RegsState -> RegsState
+setReg16 r w s = splitRegs pair r where
+    pair hr lr = setReg8 hr hi . setReg8 lr lo $ s
+    hi = highByte w
+    lo = lowByte w
